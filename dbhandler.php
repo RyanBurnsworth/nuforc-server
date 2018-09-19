bac66199062f478bd9cc81cb86015dabcc73dc09
Ryan Burnsworth <ryanburnsworth@gmail.com>
2018-09-19 04:27:19 -0400

    Update dbhandler.php

--- dbhandler.php
+++ dbhandler.php
@@ -5,31 +5,30 @@
 	 */
 	 
 	 class DbHandler {
-		
 		 private $conn;
 		 
 		 function __construct() {
-			require_once dirname(__FILE__) . '/dbconnect.php';
+		     require_once dirname(__FILE__) . '/dbconnect.php';
 			
-			$db = new DbConnect();
-			$this->conn = $db->connect();
+		     $db = new DbConnect();
+		     $this->conn = $db->connect();
 		 }
 		 
 		 public function insertSightingRecord($sightingData) {
-		    $dateTime = $sightingData->getDateTime();
-		    $city = $sightingData->getCity();
-		    $state = $sightingData->getState();
-		    $location = $sightingData->getLocation();
-		    $longitude = $sightingData->getLongitude();
-		    $latitude = $sightingData->getLatitude();
-		    $shape = $sightingData->getShape();
-		    $duration = $sightingData->getDuration();
-		    $summary = $sightingData->getSummary();
-		    $posted = $sightingData->getPosted();
+		     $dateTime = $sightingData->getDateTime();
+		     $city = $sightingData->getCity();
+		     $state = $sightingData->getState();
+		     $location = $sightingData->getLocation();
+		     $longitude = $sightingData->getLongitude();
+		     $latitude = $sightingData->getLatitude();
+		     $shape = $sightingData->getShape();
+		     $duration = $sightingData->getDuration();
+		     $summary = $sightingData->getSummary();
+		     $posted = $sightingData->getPosted();
 		    
-			$stmt = $this->conn->prepare("INSERT INTO SightingRecords(DateTime, City, State, Location, Longitude, Latitude, Shape, Duration, Summary, Posted) values('$dateTime', '$city', '$state', '$location', '$longitude', '$latitude', '$shape', '$duration', '$summary', '$posted')");
-			$stmt->execute();
-			$stmt->close();
-		}
+          	     $stmt = $this->conn->prepare("INSERT INTO SightingRecords(DateTime, City, State, Location, Longitude, Latitude, Shape, Duration, Summary, Posted) values('$dateTime', '$city', '$state', '$location', '$longitude', '$latitude', '$shape', '$duration', '$summary', '$posted')");
+	             $stmt->execute();
+		     $stmt->close();
+		 }
 	 }
 ?>
